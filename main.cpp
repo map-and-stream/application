ddeb3ac0c4d90f2b741ff7dc654ec56aa2a4c5ec
majid <majid.asgari.cs@gmail.com>
2025-10-13 21:24:37 +0330

    build app

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 // Necessary header files for input output functions
 #include <iostream>
 using namespace std;
+#include "log/logger.h"
 
 // main() function: where the execution of
 // C++ program begins
 int main() {
   
     // This statement prints "Hello World"
-    cout << "Hello World";
-
+    std::cout << "Hello World\n";
+    ConsoleLogger logger;
+    logger.Info("hello from sub-module logger");
     return 0;
 }
